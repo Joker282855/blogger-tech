28b3ba5385645996910f30006ff9800ee26d53af
unknown <jtjackson325@gmail.com>
2021-10-03 12:37:20 -0600

    got the other links to suggest signing up or signing in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,12 +35,72 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-    res.render('login');
+router.get('/dashboard', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('dashboard', {posts });
+    })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
-router.get('/', (req, res) => {
-    res.render('dashboard');
-})
+router.get('/login', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+    
+    res.render('login', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+}); 
 
 module.exports = router;
