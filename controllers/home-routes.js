21de0b3c2365a92a62ff1eb6cbd65ae542d2d0df
unknown <jtjackson325@gmail.com>
2021-10-04 15:11:39 -0600

    got the signup to go to a homepage with the correct links

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/homepage', (req, res) => {
     console.log(req.session);
 
     Post.findAll({
@@ -63,7 +63,7 @@ router.get('/dashboard', (req, res) => {
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('dashboard', {posts });
+        res.render('dashboard', { posts });
     })
         .catch(err => {
         console.log(err);
@@ -115,7 +115,37 @@ router.get('/signup', (req, res) => {
 });
 
 router.get('/signedin', (req, res) => {
-    res.render('signedin');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+    res.render('signedin', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
