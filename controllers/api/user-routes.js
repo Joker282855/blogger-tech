8b6f3123e55ea53bc09db4cacd49a379a6fe32f3
unknown <jtjackson325@gmail.com>
2021-10-05 17:35:41 -0600

    got everything working pretty much

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -89,7 +89,7 @@ router.post('/signup', (req, res) => {
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
-            res.status(204).end();
+            res.status(204).end();         
         });
     } else {
         res.status(404).end();
--- public/javascript/update.js
+++ public/javascript/update.js
@@ -1,16 +1,21 @@
-function updateFormHandler(event) {
+async function updateFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('#title-update').value.trim();
     
     if (title) {
-        fetch('/api/post/1', {
+        const response = await fetch('/api/post/1', {
             method: 'put',
             body: JSON.stringify({
                 title
             }),
             headers: { 'Content-Type': 'application/json' }
-        }).then((response) => {consnole.log(response)})
+        });
+        if (response.ok) {
+            document.location.replace('/posted');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
