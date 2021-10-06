31ff66e5ed6dc1ae83d6a43313831a35907643b1
unknown <jtjackson325@gmail.com>
2021-10-05 18:58:03 -0600

    got everything lookin good

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,40 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+    res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+})
+
 router.get('/homepage', (req, res) => {
     console.log(req.session);
 
